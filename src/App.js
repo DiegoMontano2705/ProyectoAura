41c6afc055bbd92bb7f20a91cd5351517ea7d177
Ricardo Gabriel Ramírez Arriaga <a01338143@itesm.mx>
2020-11-16 20:20:30 -0600

    Carrito funcionando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import HomeLanding from "./Components/Pricing";
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={HomeLanding}/>
+        <Route path="/" exact component={HomeLanding} />
       </Switch>
     </Router>
   );
