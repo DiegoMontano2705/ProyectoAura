a083340c42917b189dfd0d99b30113a09327a158
Diego Montaño <diegofmp98@gmail.com>
2020-11-16 23:32:09 -0600

    DB connection and Server

---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mogoURL:"mongodb+srv://m001-student:adminJoyeriaAura@joyeriaaura.mv3qt.mongodb.net/JoyeriaAura?retryWrites=true&w=majority"
+}
--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import  { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import HomeLanding from "./Components/Pricing";
+import Login from "./Components/login";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={HomeLanding}/>
+        <Route path="/login" exact component={Login}/>
       </Switch>
     </Router>
   );
