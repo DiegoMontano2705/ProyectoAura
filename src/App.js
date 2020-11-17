57c806a2daef079c595cb0c9cf7f39c9932ef30c
Andres Orozco <andresorocast.aoc@gmail.com>
2020-11-16 21:16:09 -0600

    diseno login y tablas de productos y ordenes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import  { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import HomeLanding from "./Components/Pricing";
+import AdminDashboard from "./Components/Admin";
+import AdminLogin from "./Components/LoginAdmin";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={HomeLanding}/>
+        <Route exact path = "/admin"><AdminDashboard /></Route>
+        <Route exact path = "/admin/login"><AdminLogin /></Route>
       </Switch>
     </Router>
   );
