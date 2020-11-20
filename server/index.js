1325e4a08b77e71d4929e5c29b21deb9e892d81d
Ricardo Gabriel Ramírez Arriaga <a01338143@itesm.mx>
2020-11-19 18:28:09 -0600

    server listo

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const https = require('https');
+const PORT = 8080;
+const app = express();
+
+/* JSON body parse*/
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// ------------------ firebase 
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://proyecto-a-fba61.firebaseio.com"
+});
+
+const db = admin.firestore();
+//----------start
+
+
+app.get('/hello', async (req, res, next) => {
+    const r = await db.collection('users').add({ 'name': 'ricardo' });
+
+    res.send('Welcome to Firebase Cloud Functions');
+});
+
+
+
+
+
+
+
+//---------- end
+app.listen(PORT, () => {
+    console.info('Server is running on PORT:', PORT);
+});
