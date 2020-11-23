d0865454674af4e76bd30cfa60d714ad53823054
Diego Montaño <31355652+DiegoMontano2705@users.noreply.github.com>
2020-11-22 18:44:47 -0600

    Created Contact page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from "./Components/login";
 import AdminDashboard from "./Components/Admin";
 import AdminLogin from "./Components/LoginAdmin";
 import Register from "./Components/Register";
+import Contact from "./Components/Contacto";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route exact path = "/admin"><AdminDashboard /></Route>
         <Route exact path = "/admin/login"><AdminLogin /></Route>
         <Route path="/Register" exact component={Register} />
+        <Route path="/Contacto" exact component={Contact} />
       </Switch>
     </Router>
   );
